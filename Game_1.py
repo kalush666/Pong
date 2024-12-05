758d5cd9ce2e6ab1bb1ebdc70cd1d88e249bb67c
kalush666 <jonathan.kalush@gmail.com>
2024-12-05 11:35:00 +0200

    first version

--- Game_1.py
+++ Game_1.py
@@ -1,19 +1,32 @@
 import pygame
-import pygame.draw
 
 WINDOW_WIDTH = 720
 WINDOW_HEIGHT = 720
 
 pygame.init()
-size = (WINDOW_WIDTH,WINDOW_HEIGHT)
+size = (WINDOW_WIDTH, WINDOW_HEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Game_1")
 
-WHITE = (255,255,255)
-RED = (255,0,0)
-BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
 
-screen.fill(WHITE)
+image = 'background.jpg'
+img = pygame.image.load(image)
+img = pygame.transform.scale(img, (WINDOW_WIDTH, WINDOW_HEIGHT))
+
+screen.blit(img, (0, 0))
+
+circle_radius = 50
+circle_center = (circle_radius, WINDOW_HEIGHT // 2)
+pygame.draw.circle(screen, RED, circle_center, circle_radius)
+
+ellipse_width = 100
+ellipse_height = 60
+ellipse_center = (WINDOW_WIDTH - ellipse_width // 2, WINDOW_HEIGHT // 2)
+pygame.draw.ellipse(screen, BLACK, (ellipse_center[0] - ellipse_width // 2, ellipse_center[1] - ellipse_height // 2, ellipse_width, ellipse_height))
+
 pygame.display.flip()
 
 def stay():
@@ -23,10 +36,4 @@ def stay():
             if event.type == pygame.QUIT:
                 finish = True
 
-
-image = 'background.jpg'
-img = pygame.image.load(image)
-img = pygame.transform.scale(img,(WINDOW_WIDTH,WINDOW_HEIGHT))
-screen.blit(img, (0, 0))
-pygame.display.flip()
 stay()
